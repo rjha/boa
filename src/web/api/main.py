de448bcc9fb2be406ff5e3447e244203d346c36a
Rajeev Jha <jha.rajeev@gmail.com>
2026-08-13 00:20:02 +0530

    add fastapi static mount point
    - added static mount point
    - added chat interface HTML in static folder

--- src/web/api/main.py
+++ src/web/api/main.py
@@ -1,7 +1,9 @@
 import os
+from pathlib import Path
 import logging 
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 from config import AppConfig
 from config import get_logger_config
 from .routers.llm import llm_router
@@ -22,6 +24,13 @@ async def lifespan(myapp: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 app.include_router(llm_router)
+
+# Path(__file__).resolve().parent points to 'web/api'
+# .parent again points to 'web/'
+WEB_DIR = Path(__file__).resolve().parent.parent 
+STATIC_DIR = WEB_DIR / "static"
+# Mount '/static' to serve web/static/index.html
+app.mount("/static", StaticFiles(directory=STATIC_DIR, html=True), name="static")
 
 @app.get("/hello")
 def write_hello_world():
