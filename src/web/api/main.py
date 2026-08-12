5f4beb352cab3724f96ae2c7faa1595afaaab038
Rajeev Jha <jha.rajeev@gmail.com>
2026-08-12 16:18:50 +0530

    add LLM router to fastapi interface

--- src/web/api/main.py
+++ src/web/api/main.py
@@ -4,6 +4,8 @@ from contextlib import asynccontextmanager
 from fastapi import FastAPI
 from config import AppConfig
 from config import get_database_config, get_logger_config
+from .routers.llm import llm_router
+
 
 # noinspection PyUnusedLocal
 @asynccontextmanager
@@ -19,7 +21,7 @@ async def lifespan(myapp: FastAPI):
     # shutdown events
 
 app = FastAPI(lifespan=lifespan)
-
+app.include_router(llm_router)
 
 @app.get("/hello")
 def write_hello_world():
---
+++ src/web/api/routers/llm.py
@@ -0,0 +1,32 @@
+import os 
+from fastapi import Response, status, APIRouter
+from fastapi import Request
+from openai import OpenAI
+
+
+client = OpenAI(
+    base_url="https://inference.do-ai.run/v1/",
+    api_key=os.environ.get("QWEN3_32B_KEY"),
+)
+
+
+# do not include a final slash in the prefix
+llm_router = APIRouter(prefix="/llm", tags=["LLM"], responses={404: {"description": "LLM router not found"}})
+
+# noinspection SpellCheckingInspection
+@llm_router.post("/qwen3/chat", status_code=200)
+def process_qwen3_chat(prompt: str = "Explain the CAP theorem."):
+    
+    completion = client.chat.completions.create(
+        model="alibaba-qwen3-32b",
+        messages=[{"role": "user", "content": prompt}],
+    )
+
+    # Extract the string content from the response object
+    reply_content = completion.choices[0].message.content
+
+    return {
+        "status": "success",
+        "model": "alibaba-qwen3-32b",
+        "response": reply_content,
+    }
